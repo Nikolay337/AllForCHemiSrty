4a6900d25dd800e63a2a584ee489e1f7d967fd1a
Nikolay337 <81160865+Nikolay337@users.noreply.github.com>
2023-03-16 07:51:09 +0200

    trying to fix gitignore file

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -21,7 +21,7 @@ const login = async (req, res) => {
     }
 
     const token = jwt.sign({ name: user.name, id: user.id, admin: user.admin },
-      process.env.JWT_SECRET, { expiresIn: '7d' });
+      process.env.JWT_SECRET, { expiresIn: '1d' });
 
     res.status(200).json({ token });
   } catch (err) {
